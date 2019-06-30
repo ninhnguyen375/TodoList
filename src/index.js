aa6d82e30fbd61df86f4bc0e4c2b9f115763fbe5
Ninh <ninhnguyen375@gmail.com>
2019-06-30 18:10:34 +0700

    line 2 hihi

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 console.log('hello new line')
 console.log('new line from hihi')
 console.log('new line from master')
+console.log('new line from hihi 2')
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
